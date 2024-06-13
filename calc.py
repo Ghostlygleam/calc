f54cb8fc765e908167854a9ed200f4c1ccc4835e
Vika <123155562+Ghostlygleam@users.noreply.github.com>
2024-06-13 20:29:56 +0500

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,13 @@
+https://www.figma.com/design/9QgDvbKKn3Mj53iWUxR33v/%D0%A2%D0%B5%D0%BC%D0%B0-(Copy)?node-id=0-1&t=GvvbA78FGjk0YWjx-1
+
+
+https://unsplash.com/
+
+https://stocksnap.io 
+
+https://www.pexels.com 
+
+https://flickr.com/ 
+
+https://freerangestock.com/
+
