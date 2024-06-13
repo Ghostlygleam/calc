f91a248f99436bde7d9ebb51eeed23d4f348c7f8
Vika <123155562+Ghostlygleam@users.noreply.github.com>
2024-06-13 20:39:17 +0500

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,4 +1,4 @@
-https://www.figma.com/design/9QgDvbKKn3Mj53iWUxR33v/%D0%A2%D0%B5%D0%BC%D0%B0-(Copy)?node-id=0-1&t=GvvbA78FGjk0YWjx-1
+https://www.figma.com/design/9QgDvbKKn3Mj53iWUxR33v/New?node-id=0-1&t=GvvbA78FGjk0YWjx-1
 
 
 https://unsplash.com/
